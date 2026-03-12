7e7a03e4e06f09a7025c21ae3bec1716aab8613c
hank <azsx1539004222@gmail.com>
2026-03-12 17:01:35 +0800

    for init

---
+++ Lab01/b.js
@@ -0,0 +1,4 @@
+(function (){
+    let a = 1000;
+    return a;
+})
---
+++ Lab01/c.js
@@ -0,0 +1,11 @@
+// 命名匯出
+// 可以只匯出部分
+// export let a = 2000;
+// export function add(a,b){
+//     return a + b;
+// }
+
+// 預設匯出
+export default function greeting(name){
+    return `hello ${name}`;
+}
